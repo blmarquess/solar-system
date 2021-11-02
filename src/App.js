69a2b9f6dda13fdae20784bc5d9cf94bc45eb06d
Bruno <blmarques.dev@gmail.com>
2021-11-02 04:29:03 -0300

    Add: Stories

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
     <>
       <div className="border w-2/3 m-auto">
         <header> Sistema Solar </header>
-      
+
         <div className="flex p-4 space-x-4 border shadow-sm overflow-x-auto ...">
           <div className="border-2 rounded-full border-pink-600 w-20 h-20">
             <div className="w-20 h-20 rounded-full border-2">
               <span> Enviar </span>
             </div>
           </div>
-            
+
           {data.map((item) => <Stories srcImage={item.image} name={item.name} />)}
         </div>
       </div>
