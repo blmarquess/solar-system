db01c235a642acb42e973fda65c3c50764d5137c
Bruno <blmarques.dev@gmail.com>
2021-11-02 04:27:28 -0300

    Add: Stories component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import './App.css';
 import './index.css';
+import Stories from './components/Stories';
+import data from './data/planets';
 
 function App() {
   return (
-    <div className="App">
-        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-          <div class="flex-shrink-0">
-            <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
-                </div>
-            <div>
-            <div class="text-xl font-medium text-black">ChitChat</div>
-            <p class="text-gray-500">You have a new message!</p>
+    <>
+      <div className="border w-2/3 m-auto">
+        <header> Sistema Solar </header>
+      
+        <div className="flex p-4 space-x-4 border shadow-sm overflow-x-auto ...">
+          <div className="border-2 rounded-full border-pink-600 w-20 h-20">
+            <div className="w-20 h-20 rounded-full border-2">
+              <span> Enviar </span>
+            </div>
           </div>
+            
+          {data.map((item) => <Stories srcImage={item.image} name={item.name} />)}
+        </div>
       </div>
-      <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-  <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face" />
-  <div class="text-center space-y-2 sm:text-left">
-    <div class="space-y-0.5">
-      <p class="text-lg text-black font-semibold">
-        Erin Lindford
-      </p>
-      <p class="text-gray-500 font-medium">
-        Product Engineer
-      </p>
-    </div>
-    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-  </div>
-</div>
-    </div>
+    </>
   );
 }
 
